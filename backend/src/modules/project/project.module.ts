2408893b751e312f059002597648a8d1ddd76293
Yuliia Maiboroda <yyuulliiaa45@gmail.com>
2025-03-06 18:06:10 +0200

    feat/react-init

--- backend/src/modules/project/project.module.ts
+++ backend/src/modules/project/project.module.ts
@@ -3,9 +3,19 @@ import { ProjectService } from './project.service';
 import { ProjectController } from './project.controller';
 import { TypeOrmModule } from '@nestjs/typeorm';
 import { Project } from 'src/entities/postgres/project.entity';
+import { MongooseModule } from '@nestjs/mongoose';
+import {
+  UserSession,
+  UserSessionSchema,
+} from 'src/entities/mongoDB/user-session.entity';
 
 @Module({
-  imports: [TypeOrmModule.forFeature([Project])],
+  imports: [
+    TypeOrmModule.forFeature([Project]),
+    MongooseModule.forFeature([
+      { name: UserSession.name, schema: UserSessionSchema },
+    ]),
+  ],
   controllers: [ProjectController],
   providers: [ProjectService],
   exports: [ProjectService],
---
+++ frontend/vite.config.ts
@@ -0,0 +1,33 @@
+import dns from "dns";
+import { URL, fileURLToPath } from "node:url";
+
+import react from "@vitejs/plugin-react";
+import { defineConfig, loadEnv } from "vite";
+import checker from "vite-plugin-checker";
+import mkcert from "vite-plugin-mkcert";
+import tsconfigPaths from "vite-tsconfig-paths";
+
+dns.setDefaultResultOrder("verbatim");
+
+export default defineConfig(({ mode }) => {
+  const env = loadEnv(mode, process.cwd());
+
+  return {
+    base: env.VITE_BASE_URL,
+    plugins: [
+      react(),
+      tsconfigPaths(),
+      mkcert(),
+      checker({ typescript: true }),
+    ],
+    resolve: {
+      alias: {
+        "@": fileURLToPath(new URL("./src", import.meta.url)),
+      },
+    },
+    server: {
+      open: true,
+      host: true,
+    },
+  };
+});
