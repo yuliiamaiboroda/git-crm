96f9d603f46bbb66982e1644a2f29692378cd636
Yuliia Maiboroda <yyuulliiaa45@gmail.com>
2025-03-06 15:36:37 +0200

    feat:auth-guards

---
+++ backend/src/guards/auth.guard.ts
@@ -0,0 +1,75 @@
+import {
+  CanActivate,
+  ExecutionContext,
+  Global,
+  Injectable,
+  UnauthorizedException,
+} from '@nestjs/common';
+import { InjectModel } from '@nestjs/mongoose';
+
+import { Request } from 'express';
+import * as argon2 from 'argon2';
+import { Model } from 'mongoose';
+
+import { UserService } from 'src/modules/user/user.service';
+import { AuthService } from 'src/modules/auth/auth.service';
+
+import { User } from 'src/entities/postgres/user.entity';
+import { UserSession } from 'src/entities/mongoDB/user-session.entity';
+
+import { UserTokenData } from 'src/types/auth.types';
+
+@Global()
+@Injectable()
+export class AuthGuard implements CanActivate {
+  constructor(
+    @InjectModel(UserSession.name) private userSessionModel: Model<UserSession>,
+
+    private authService: AuthService,
+    private userService: UserService,
+  ) {}
+
+  async canActivate(context: ExecutionContext): Promise<boolean> {
+    const request = context.switchToHttp().getRequest();
+    const token = this.extractTokenFromHeader(request);
+    if (!token) {
+      throw new UnauthorizedException();
+    }
+    try {
+      const payload: UserTokenData = this.authService.verifyUserToken(token);
+      const user: User = await this.validateUserInDatabase(payload);
+      await this.validateUserSession(payload);
+
+      request['user'] = user;
+    } catch {
+      throw new UnauthorizedException();
+    }
+    return true;
+  }
+
+  private extractTokenFromHeader(request: Request): string | undefined {
+    const [type, token] = request.headers.authorization?.split(' ') ?? [];
+    return type === 'Bearer' ? token : undefined;
+  }
+
+  private async validateUserInDatabase(payload: UserTokenData): Promise<User> {
+    const user: User = await this.userService.findById(payload.id);
+    if (!user) throw new UnauthorizedException();
+
+    const isPasswordValid = await argon2.verify(
+      user.password,
+      payload.password,
+    );
+    if (!isPasswordValid) throw new UnauthorizedException();
+
+    return user;
+  }
+
+  private async validateUserSession(payload: UserTokenData): Promise<void> {
+    const userSession: UserSession = await this.userSessionModel.findOne({
+      userId: payload.id,
+      secret: payload.secret,
+    });
+    if (!userSession) throw new UnauthorizedException();
+  }
+}
--- backend/src/modules/auth/auth.controller.ts
+++ backend/src/modules/auth/auth.controller.ts
@@ -1,14 +1,18 @@
-import { Body, Controller, Post } from '@nestjs/common';
+import { Body, Controller, Post, UseGuards } from '@nestjs/common';
 import { AuthService } from './auth.service';
 import { AuthDto } from './dto';
 import { AuthResponse } from './responses';
 import {
+  ApiBearerAuth,
   ApiBody,
   ApiOperation,
   ApiResponse,
   ApiTags,
   ApiUnauthorizedResponse,
 } from '@nestjs/swagger';
+import { AuthGuard } from 'src/guards/auth.guard';
+import { User } from 'src/entities/postgres/user.entity';
+import { CurrentUser } from 'src/decorators/user.decorator';
 
 @ApiTags('auth')
 @Controller('auth')
@@ -31,5 +35,15 @@ export class AuthController {
   @Post('register')
   async register(@Body() authDto: AuthDto): Promise<AuthResponse> {
     return this.authService.register(authDto);
+  }
+
+  @ApiBearerAuth()
+  @ApiOperation({ summary: 'Logout' })
+  @ApiResponse({ status: 204 })
+  @ApiUnauthorizedResponse({ description: 'Unauthorized' })
+  @UseGuards(AuthGuard)
+  @Post('logout')
+  async logout(@CurrentUser() user: User): Promise<void> {
+    return this.authService.logout(user.id);
   }
 }
